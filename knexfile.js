07d76db7db71dd48c440b15898c88d9defc8cbd3
Cauã Henrique <cauah6002@gmail.com>
2021-06-18 18:22:56 -0300

    Fixed Knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,17 @@
 // Update with your config settings.
 
 module.exports = {
+  client: 'pg',
+  connection: {
+    host: 'ec2-54-164-241-193.compute-1.amazonaws.com',
+    database: 'd68vinefp82nsr',
+    user: 'gvclarjnfymigb',
+    password: 'a7b8c21a2378e0871aafc570f26e468cb29fbee57eec9a6a3f7661eb64470002'
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+  /*
   development: {
     client: 'pg',
     connection: {
@@ -19,4 +30,5 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
+  */
 };
