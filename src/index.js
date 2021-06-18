278744afc7a0135f53e72d9986fec62997fb3582
Cauã Henrique <cauah6002@gmail.com>
2021-06-18 18:31:51 -0300

    Fixed port

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
-app.listen(3001, () => console.log('[BACKEND] Rodando'))
+app.listen(process.env.PORT || 3001, () => console.log('[BACKEND] Rodando'))
