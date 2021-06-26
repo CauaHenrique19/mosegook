46588c790bd19d250cfc3fdf5d9e9c471c358ff4
Cauã Henrique <cauah6002@gmail.com>
2021-06-25 23:51:52 -0300

    Adding query to return most good rateds medias

--- src/Controllers/MediasController.js
+++ src/Controllers/MediasController.js
@@ -3,26 +3,26 @@ const aws = require('aws-sdk')
 
 const s3 = new aws.S3()
 
-class MediasController{
-    async create(req, res){
+class MediasController {
+    async create(req, res) {
         const { name, synopsis, category_id, avaliation } = req.body
         const { key: key_poster, location: url_poster } = req.files[0]
         const { key: key_poster_timeline, location: url_poster_timeline } = req.files[1]
-        
+
         const media = { name, synopsis, category_id, avaliation, key_poster, url_poster, key_poster_timeline, url_poster_timeline }
         const { genders } = req.body
 
         const mediaDB = await knex('medias')
             .insert(media, '*')
-        
-        const objectsToInsert = genders.map(genderId => { return { gender_id: genderId, media_id: mediaDB[0].id }})
+
+        const objectsToInsert = genders.map(genderId => { return { gender_id: genderId, media_id: mediaDB[0].id } })
 
         const gendersInMediaInDb = await knex('genders_in_medias')
             .insert(objectsToInsert, 'gender_id')
 
         const gendersDB = []
 
-        for(let i = 0; i < gendersInMediaInDb.length; i++){
+        for (let i = 0; i < gendersInMediaInDb.length; i++) {
             const data = await knex('genders')
                 .select('*')
                 .where({ id: gendersInMediaInDb[i] })
@@ -34,51 +34,51 @@ class MediasController{
         mediaDB[0].genders = gendersDB
         res.json(mediaDB)
     }
-    async index(req, res){
+    async index(req, res) {
         const medias = await knex('medias')
-            .select('medias.id', 'medias.name', 'medias.synopsis', 
-                    'medias.category_id', 'medias.avaliation', 'medias.key_poster',
-                    'medias.url_poster', 'medias.key_poster_timeline', 'medias.url_poster_timeline',
-                    'categories.name as category_name', 'categories.color as category_color', 'categories.icon as category_icon')
+            .select('medias.id', 'medias.name', 'medias.synopsis',
+                'medias.category_id', 'medias.avaliation', 'medias.key_poster',
+                'medias.url_poster', 'medias.key_poster_timeline', 'medias.url_poster_timeline',
+                'categories.name as category_name', 'categories.color as category_color', 'categories.icon as category_icon')
             .join('categories', 'categories.id', 'medias.category_id')
             .orderBy('id')
 
-        for(let i = 0; i < medias.length; i++){
+        for (let i = 0; i < medias.length; i++) {
             const gendersOfMedias = await knex('genders_in_medias')
-                .select('genders_in_medias.id as genders_in_medias_id','genders_in_medias.gender_id as id', 'genders.name', 'genders.color')
+                .select('genders_in_medias.id as genders_in_medias_id', 'genders_in_medias.gender_id as id', 'genders.name', 'genders.color')
                 .join('genders', 'genders.id', 'genders_in_medias.gender_id')
                 .where({ media_id: medias[i].id })
 
             medias[i].genders = gendersOfMedias
         }
-        
+
         res.json(medias)
     }
-    async byCategories(req, res){
+    async byCategories(req, res) {
         const categoryId = req.params.id
 
         const medias = await knex('medias')
-            .select('medias.id', 'medias.name', 'medias.synopsis', 
-                    'medias.category_id', 'medias.avaliation', 'medias.key_poster',
-                    'medias.url_poster', 'medias.key_poster_timeline', 'medias.url_poster_timeline',
-                    'categories.name as category_name', 'categories.color as category_color', 'categories.icon as category_icon')
+            .select('medias.id', 'medias.name', 'medias.synopsis',
+                'medias.category_id', 'medias.avaliation', 'medias.key_poster',
+                'medias.url_poster', 'medias.key_poster_timeline', 'medias.url_poster_timeline',
+                'categories.name as category_name', 'categories.color as category_color', 'categories.icon as category_icon')
             .join('categories', 'categories.id', 'medias.category_id')
             .orderBy('id')
-            .where({ category_id : categoryId })
+            .where({ category_id: categoryId })
 
-        for(let i = 0; i < medias.length; i++){
-            if(medias){
+        for (let i = 0; i < medias.length; i++) {
+            if (medias) {
                 const gendersOfMedias = await knex('genders_in_medias')
-                    .select('genders_in_medias.id as genders_in_medias_id','genders_in_medias.gender_id as id', 'genders.name', 'genders.color')
+                    .select('genders_in_medias.id as genders_in_medias_id', 'genders_in_medias.gender_id as id', 'genders.name', 'genders.color')
                     .join('genders', 'genders.id', 'genders_in_medias.gender_id')
                     .where({ media_id: medias[i].id })
-                    medias[i].genders = gendersOfMedias
+                medias[i].genders = gendersOfMedias
             }
         }
 
         res.json(medias)
     }
-    async statistics(req, res){
+    async statistics(req, res) {
         const categoryId = req.params.categoryId
 
         const mostRated = await knex('avaliations')
@@ -96,10 +96,22 @@ class MediasController{
             .join('medias', 'medias.id', 'avaliations.media_id')
             .where({ category_id: categoryId })
             .groupBy('medias.category_id', 'category_id')
-        
-        return res.json({mostRated, amountAvaliations})
+
+        const mostGoodRated = await knex.raw(`
+            select
+                to_char(avg(avaliations.stars), 'FM999999999.0') as media_stars,
+                medias.name
+            from avaliations
+            inner join medias on medias.id = avaliations.media_id
+            where medias.category_id = ${categoryId}
+            group by 
+                avaliations.media_id, medias.name
+            order by media_stars DESC 
+            limit 1;`)
+
+        return res.json({ mostRated, amountAvaliations, mostGoodRated: mostGoodRated.rows })
     }
-    async delete(req, res){
+    async delete(req, res) {
         const mediaId = req.params.id
 
         const mediaInDB = await knex('medias')
@@ -107,7 +119,7 @@ class MediasController{
             .where({ id: mediaId })
             .first()
 
-        const objectsToDelete = Object.values(mediaInDB).map(key => { return { Key: key }})
+        const objectsToDelete = Object.values(mediaInDB).map(key => { return { Key: key } })
 
         s3.deleteObjects({
             Bucket: 'mosegook',
