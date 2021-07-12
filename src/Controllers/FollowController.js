65ed923f6221931dad4d3894c89dbd99e95275e0
Cauã Henrique <cauah6002@gmail.com>
2021-07-12 10:27:50 -0300

    Randomizing get users to follow and adding try catch block in users controller

--- src/Controllers/FollowController.js
+++ src/Controllers/FollowController.js
@@ -2,37 +2,53 @@ const knex = require("../database/connection")
 
 class FollowController{
     async create(req, res){
-        const { user_id, following_user_id } = req.body
-
-        const follow = {
-            user_id, 
-            following_user_id,
-            created_at: new Date().toLocaleString()
+        try{
+            const { user_id, following_user_id } = req.body
+    
+            const follow = {
+                user_id, 
+                following_user_id,
+                created_at: new Date().toLocaleString()
+            }
+    
+            const followDB = await knex('follow')
+                .insert(follow, '*')
+            
+            res.json(followDB)
         }
-
-        const followDB = await knex('follow')
-            .insert(follow, '*')
-        
-        res.json(followDB)
+        catch(error){
+            return res.status(500).json({ message: 'Ocorreu um erro inesperado!', error: error.message })
+        }
     }
     async usersToFollow(req, res){
-        const { id } = req.params
-
-        const users = await knex('users')
-            .select('id', 'name', 'user', 'url_image')
-            .whereNot({ id })
-            .limit(5)
-
-        return res.json(users)
+        try{
+            const { id } = req.params
+    
+            const users = await knex.raw(`
+                select id, name, users.user, url_image from users
+                where not id = ${id}
+                order by random()
+                limit 5`)
+    
+            return res.json(users.rows)
+        }
+        catch(error){
+            return res.status(500).json({ message: 'Ocorreu um erro inesperado!', error: error.message })
+        }
     }
     delete(req, res){
-        const { user_id, following_user_id } = req.body
-
-        knex('follow')
-            .delete()
-            .where({ user_id, following_user_id })
-            .then(_ => res.json({ message: 'Você deixou de seguir este usuário!' }))
-            .catch(error => res.status(500).json({ message: 'Erro inesperado ao tentar deixar de seguir usuário!', error: error.message }))
+        try{
+            const { user_id, following_user_id } = req.body
+    
+            knex('follow')
+                .delete()
+                .where({ user_id, following_user_id })
+                .then(_ => res.json({ message: 'Você deixou de seguir este usuário!' }))
+                .catch(error => res.status(500).json({ message: 'Erro inesperado ao tentar deixar de seguir usuário!', error: error.message }))
+        }
+        catch(error){
+            return res.status(500).json({ message: 'Ocorreu um erro inesperado!', error: error.message })
+        }
     }
 }
 
