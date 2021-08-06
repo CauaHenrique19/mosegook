bf530a2e4f2fee973ca26675ee504c0a34f8f2ae
CauaHenrique19 <cauah6002@gmail.com>
2021-08-05 22:38:56 -0300

    Adding try catch blocks in users preferences medias controller

--- src/Controllers/UserPreferencesMediasController.js
+++ src/Controllers/UserPreferencesMediasController.js
@@ -2,38 +2,53 @@ const knex = require("../database/connection")
 
 class UserPreferencesMediasController{
     async create(req, res){
-        const { user_id, media_id } = req.body
-
-        if(!user_id) return res.status(400).json({ message: 'Informe o id do usuário' })
-        if(!media_id) return res.status(400).json({ message: 'Informe o id da mídia' })
-
-        const objectsToInsert = media_id.map(media => { return { user_id, media_id: media } })
-
-        const mediasPreferenceDb = await knex('user_preferences_medias')
-            .insert(objectsToInsert, '*')
-
-        return res.json(mediasPreferenceDb)
+        try{
+            const { user_id, media_id } = req.body
+    
+            if(!user_id) return res.status(400).json({ message: 'Informe o id do usuário' })
+            if(!media_id) return res.status(400).json({ message: 'Informe o id da mídia' })
+    
+            const objectsToInsert = media_id.map(media => { return { user_id, media_id: media } })
+    
+            const mediasPreferenceDb = await knex('user_preferences_medias')
+                .insert(objectsToInsert, '*')
+    
+            return res.json(mediasPreferenceDb)
+        }
+        catch(error){
+            return res.status(500).json({ message: 'Ocorreu um erro ao criar preferencias por mídias', error: error.message })
+        }
     }
     async index(req, res){
-        const { user_id } = req.body
-
-        if(!user_id) return res.status(400).json({ message: 'Informe o id do usuário' })
-
-        const mediasPreferences = await knex('user_preferences_medias')
-            .select('user_preferences_medias.id', 'user_preferences_medias.user_id', 'medias.name')
-            .join('medias', 'user_preferences_medias.media_id', 'medias.id')
-            .where({ user_id })
-
-        return res.json(mediasPreferences)
+        try{
+            const { user_id } = req.body
+    
+            if(!user_id) return res.status(400).json({ message: 'Informe o id do usuário' })
+    
+            const mediasPreferences = await knex('user_preferences_medias')
+                .select('user_preferences_medias.id', 'user_preferences_medias.user_id', 'medias.name')
+                .join('medias', 'user_preferences_medias.media_id', 'medias.id')
+                .where({ user_id })
+    
+            return res.json(mediasPreferences)
+        }
+        catch(error){
+            return res.status(500).json({ message: 'Ocorreu um erro ao pegar preferencias por mídias', error: error.message })
+        }
     }
     delete(req, res){
-        const userPreferenceId = req.params.id
-
-        knex('user_preferences_medias')
-            .delete()
-            .where({ id: userPreferenceId })
-            .then(() => res.json({ message: 'Preferência excluída com sucesso!' }))
-            .catch(error => res.status(500).json({ message: 'Ocorreu um erro inesperado ao tentar excluir preferência', error: error.message }))
+        try{
+            const userPreferenceId = req.params.id
+    
+            knex('user_preferences_medias')
+                .delete()
+                .where({ id: userPreferenceId })
+                .then(() => res.json({ message: 'Preferência excluída com sucesso!' }))
+                .catch(error => res.status(500).json({ message: 'Ocorreu um erro inesperado ao tentar excluir preferência', error: error.message }))
+        }
+        catch(error){
+            return res.status(500).json({ message: 'Ocorreu um erro ao pegar preferencias por mídias', error: error.message })
+        }
     }
 }
 
